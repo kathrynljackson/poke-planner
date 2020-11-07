4fd894703153fc10171cbbbd44d57faacdefd050
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-11-06 19:15:35 -0500

    Add info for all gym leaders

---
+++ src/GymLeaders/GymLeaders.js
@@ -0,0 +1,386 @@
+import React from 'react';
+import './GymLeaders.scss';
+
+
+
+export default opponentInfo = 
+    [
+        {
+        "image": "./GymLeader-images/milo-game.jpg",
+        "order": 1,
+        "gym": "Turffield Gym",
+        "leader": "Milo",
+        "type": "Grass",
+        "pokemon": [
+            {
+                "pokemonName": "Gossifluer",
+                "pokemonId": 829, 
+                "pokemonLevel": 19,
+                "pokemonType": "Grass",
+                "pokemonWeakness": "Fire, Flying, Ice, Poison, and Bug"
+            },
+                        {
+                "pokemonName": "Eldegoss",
+                "pokemonId": 830, 
+                "pokemonLevel": 20,
+                "pokemonType": "Grass",
+                "pokemonWeakness": "Fire, Flying, Ice, Poison, and Bug"
+            }
+        ]
+
+        
+        },
+        {
+            "image": "",
+            "order": 2,
+            "gym": "Hulbury Gym",
+            "leader": "Nessa",
+            "type": "Water",
+            "pokemon": [
+                {
+                    "pokemonName": "Goldeen",
+                    "pokemonId": 118, 
+                    "pokemonLevel": 20,
+                    "pokemonType": "Water",
+                    "pokemonWeakness": "Electric, Grass"
+                },
+                {
+                    "pokemonName": "Arrokuda",
+                    "pokemonId": 846, 
+                    "pokemonLevel": 23,
+                    "pokemonType": "Water",
+                    "pokemonWeakness": "Electric, Grass"
+                },
+                {
+                    "pokemonName": "Drednaw",
+                    "pokemonId": 834, 
+                    "pokemonLevel": 24,
+                    "pokemonType": "Water, Rock",
+                    "pokemonWeakness": "Electric, Grass, Fighting, Ground"
+                }
+            ]
+    
+            
+        },  
+        {
+            "image": "",
+            "order": 3,
+            "gym": "Motostoke Gym",
+            "leader": "Kabu",
+            "type": "Fire",
+            "pokemon": [
+                {
+                    "pokemonName": "Ninetales",
+                    "pokemonId": 38, 
+                    "pokemonLevel": 25,
+                    "pokemonType": "Fire",
+                    "pokemonWeakness": "Water, Rock, Ground"
+                },
+                {
+                    "pokemonName": "Arcanine",
+                    "pokemonId": 59, 
+                    "pokemonLevel": 25,
+                    "pokemonType": "Fire",
+                    "pokemonWeakness": "Water, Rock, Ground"
+                },
+                {
+                    "pokemonName": "Centiskorch",
+                    "pokemonId": 851, 
+                    "pokemonLevel": 27,
+                    "pokemonType": "Fire / Bug",
+                    "pokemonWeakness": "Water, Rock, Ground"
+                }
+            ]
+    
+            
+        },
+        {
+            "image": "",
+            "order": 4,
+            "gym": "Stow-on-Side Gym: Pokemon Shield",
+            "leader": "Allister",
+            "type": "Ghost",
+            "pokemon": [
+                {
+                    "pokemonName": "Yamask",
+                    "pokemonId": 562, 
+                    "pokemonLevel": 34,
+                    "pokemonType": "Ground / Ghost",
+                    "pokemonWeakness": "Dark, Steel, Water"
+                },
+                {
+                    "pokemonName": "Cursola",
+                    "pokemonId": 864, 
+                    "pokemonLevel": 35,
+                    "pokemonType": "Ghost",
+                    "pokemonWeakness": "Dark, Ghost"
+                },
+                {
+                    "pokemonName": "Mimikyu",
+                    "pokemonId": 778, 
+                    "pokemonLevel": 34,
+                    "pokemonType": "Ghost",
+                    "pokemonWeakness": "Ghost, Steel"
+                },
+                {
+                    "pokemonName": "Gengar",
+                    "pokemonId": 94, 
+                    "pokemonLevel": 36,
+                    "pokemonType": "Ghost",
+                    "pokemonWeakness": "Ghost, Dark, Psychic"
+                }
+            ]
+    
+            
+        },
+        {
+            "image": "",
+            "order": 4,
+            "gym": "Stow-on-Side Gym: Pokemon Sword",
+            "leader": "Bea",
+            "type": "Fighting",
+            "pokemon": [
+                {
+                    "pokemonName": "Hitmontop ",
+                    "pokemonId": 237, 
+                    "pokemonLevel": 34,
+                    "pokemonType": "Fighting",
+                    "pokemonWeakness": "Psychic, Flying, Fairy"
+                },
+                {
+                    "pokemonName": "Pangoro",
+                    "pokemonId": 675, 
+                    "pokemonLevel": 34,
+                    "pokemonType": "Fighting, Dark",
+                    "pokemonWeakness": "Psychic, Flying, Fairy"
+                },
+                {
+                    "pokemonName": "Sirfetch'd",
+                    "pokemonId": 865, 
+                    "pokemonLevel": 35,
+                    "pokemonType": "Fighting",
+                    "pokemonWeakness": "Psychic, Flying, Fairy"
+                },
+                {
+                    "pokemonName": "Machamp",
+                    "pokemonId": 68, 
+                    "pokemonLevel": 36,
+                    "pokemonType": "Fighting",
+                    "pokemonWeakness": "Psychic, Flying, Fairy"
+                }
+            ]
+    
+            
+        },
+        {
+            "image": "",
+            "order": 5,
+            "gym": "Ballonlea Gym",
+            "leader": "Opal",
+            "type": "Fairy",
+            "pokemon": [
+                {
+                    "pokemonName": "Weezing",
+                    "pokemonId": 110, 
+                    "pokemonLevel": 36,
+                    "pokemonType": "Poison, Fairy",
+                    "pokemonWeakness": "Psychic, Steel"
+                },
+                {
+                    "pokemonName": "Mawile",
+                    "pokemonId": 303, 
+                    "pokemonLevel": 36,
+                    "pokemonType": "Fairy, Steel",
+                    "pokemonWeakness": "Ground, Fire"
+                },
+                {
+                    "pokemonName": "Togekiss",
+                    "pokemonId": 468, 
+                    "pokemonLevel": 37,
+                    "pokemonType": " Fairy, Flying",
+                    "pokemonWeakness": "Electric, Rock, Ice, Poison, Steel"
+                },
+                {
+                    "pokemonName": "Alcremie",
+                    "pokemonId": 869, 
+                    "pokemonLevel": 38,
+                    "pokemonType": "Fairy",
+                    "pokemonWeakness": "Dark, Poison, Steel"
+                }
+            ]
+    
+            
+        },
+        {
+            "image": "",
+            "order": 6,
+            "gym": "Circhester Gym: Pokemon Shield",
+            "leader": "Melony",
+            "type": "Ice",
+            "pokemon": [
+                {
+                    "pokemonName": "Frosmoth",
+                    "pokemonId": 873, 
+                    "pokemonLevel": 40,
+                    "pokemonType": "Ice, Bug",
+                    "pokemonWeakness": "Fire"
+                },
+                {
+                    "pokemonName": "Darmanitan",
+                    "pokemonId": 555, 
+                    "pokemonLevel": 40,
+                    "pokemonType": "Ice",
+                    "pokemonWeakness": "Fighting, Steel, Fire, Rock"
+                },
+                {
+                    "pokemonName": "Eiscue",
+                    "pokemonId": 875, 
+                    "pokemonLevel": 41,
+                    "pokemonType": "Ice",
+                    "pokemonWeakness": "Fighting, Steel, Fire, Rock"
+                },
+                {
+                    "pokemonName": "Lapras",
+                    "pokemonId": 131, 
+                    "pokemonLevel": 42,
+                    "pokemonType": "Water, Ice",
+                    "pokemonWeakness": "Electric, Rock, Fighting, Grass"
+                }
+            ]
+    
+            
+        },
+        {
+            "image": "",
+            "order": 6,
+            "gym": "Circhester Gym: Pokemon Sword",
+            "leader": "Gordie",
+            "type": "Rock",
+            "pokemon": [
+                {
+                    "pokemonName": "Barbaracle",
+                    "pokemonId": 689, 
+                    "pokemonLevel": 40,
+                    "pokemonType": "Rock, Water",
+                    "pokemonWeakness": "Electric, Grass, Fighting, Ground"
+                },
+                {
+                    "pokemonName": "Shuckle",
+                    "pokemonId": 231, 
+                    "pokemonLevel": 40,
+                    "pokemonType": "Bug, Rock",
+                    "pokemonWeakness": "Water, Bug, Steel"
+                },
+                {
+                    "pokemonName": "Stonjourner",
+                    "pokemonId": 874, 
+                    "pokemonLevel": 41,
+                    "pokemonType": "Rock",
+                    "pokemonWeakness": "Ground, Fighting, Grass, Ice"
+                },
+                {
+                    "pokemonName": "Coalossal",
+                    "pokemonId": 839, 
+                    "pokemonLevel": 42,
+                    "pokemonType": "Water, Fighting, Ground",
+                    "pokemonWeakness": "Water, Fighting, Ground"
+                }
+            ]
+    
+            
+        },
+        {
+            "image": "",
+            "order": 7,
+            "gym": "Spikemuth Gym",
+            "leader": "Piers",
+            "type": "Dark",
+            "pokemon": [
+                {
+                    "pokemonName": "Scrafty",
+                    "pokemonId": 560, 
+                    "pokemonLevel": 44,
+                    "pokemonType": "Fighting, Dark",
+                    "pokemonWeakness": "Fighting, Flying, Fairy"
+                },
+                {
+                    "pokemonName": "Malamar",
+                    "pokemonId": 687, 
+                    "pokemonLevel": 45,
+                    "pokemonType": "Psychic, Dark",
+                    "pokemonWeakness": "Bug, Fairy"
+                },
+                {
+                    "pokemonName": "Obstagoon",
+                    "pokemonId": 862, 
+                    "pokemonLevel": 46,
+                    "pokemonType": "Dark, Normal",
+                    "pokemonWeakness": "Fighting, Fairy"
+                },
+                {
+                    "pokemonName": "Skuntank",
+                    "pokemonId": 435, 
+                    "pokemonLevel": 45,
+                    "pokemonType": "Poison, Dark",
+                    "pokemonWeakness": "Ground"
+                }
+            ]
+    
+            
+        },
+        {
+            "image": "",
+            "order": 8,
+            "gym": "Hammerlocke Gym",
+            "leader": "Raihan",
+            "type": "Dragon",
+            "pokemon": [
+                {
+                    "pokemonName": "Flygon",
+                    "pokemonId": 330, 
+                    "pokemonLevel": 47,
+                    "pokemonType": "Ground / Dragon",
+                    "pokemonWeakness": "Ice, Fairy, Dragon"
+                },
+                {
+                    "pokemonName": "Gigalith",
+                    "pokemonId": 526, 
+                    "pokemonLevel": 46,
+                    "pokemonType": "Rock",
+                    "pokemonWeakness": "Water, Grass, Fighting, Ground, Steel"
+                },
+                {
+                    "pokemonName": "Sandaconda",
+                    "pokemonId": 844, 
+                    "pokemonLevel": 46,
+                    "pokemonType": "Ground",
+                    "pokemonWeakness": "Flying, Bug, Grass"
+                },
+                {
+                    "pokemonName": "Duraludon",
+                    "pokemonId": 884, 
+                    "pokemonLevel": 48,
+                    "pokemonType": "Steel, Dragon",
+                    "pokemonWeakness": "Ice, Fairy"
+                }
+            ]
+    
+            
+        }
+    ]
+
+export default function GymLeaders() {
+    return (
+        <div className="gym-leaders-main">
+            {opponentInfo.map(opponent => {
+                return (
+                    <section className="opponent-card" key={opponent.leader}>
+                        
+                    </section>
+                )
+            })}
+        </div>
+    )
+}
+
+
