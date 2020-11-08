4976a4b49f2c4e2a15a8a6b29fed02ad344cee8b
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-11-07 20:02:10 -0500

    render createATeam component

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import GymLeaders from '../GymLeaders/GymLeaders';
+import CreateATeam from '../CreateATeam/CreateATeam.js';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      myTeams: []
     }
+  }
+
+  componentDidMount() {
+    this.getTeams();
+  }
+
+  getTeams = () => {
+    if (JSON.parse(localStorage.getItem('teams'))) {
+      this.setState({ TeamsEntries: JSON.parse(localStorage.getItem('teams')) })
+    }
+  }
+
+  saveTeams = () => {
+    localStorage.setItem('teams', JSON.stringify(this.state.myTeams));
+  }
+
+  updateState = (value) => {
+    this.setState({ myTeams: value })
+    this.saveTeams()
   }
 
   render() {
@@ -18,6 +38,11 @@ class App extends Component {
           <Route exact path='/gymleaders'>
             <GymLeaders />
           </Route>
+          <Route exact path='/createateam' render={() => (
+            <CreateATeam myTeams={this.state.myTeams} />
+          )}/>
+            
+          
         </div>
       </Router>
     );
