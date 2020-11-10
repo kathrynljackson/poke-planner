98c9474874437d7bc2483cc3a79ad1d0bb635a72
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-11-09 20:32:24 -0500

    Refactor

--- src/GymLeaders/GymLeaders.js
+++ src/GymLeaders/GymLeaders.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './GymLeaders.scss';
-import { PropTypes } from 'prop-types';
 
 export const opponentInfo = [
         {
